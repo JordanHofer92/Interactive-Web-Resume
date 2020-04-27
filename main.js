08890662cd75ecfc1e9c5f35a96c600c558ef154
JordanHofer92 <jhofer@ualberta.ca>
2020-04-27 10:13:55 -0600

    Graphs added to softskills, early styles applied to text

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
  //Charts for soft-skills section//
 //////////////////////////////////
 
-let ctx = document.getElementById('remote-work-graph').getContext('2d');
-let chart = new Chart(ctx, {
+let remoteCtx = document.getElementById('remote-work-graph').getContext('2d');
+let remoteChart = new Chart(remoteCtx, {
     type: 'doughnut',
     data: {
-        labels: ['Level of Proficiency', 'Room to Grow'],
+        labels: ['Proficiency', 'Growth'],
         datasets: [{
             label: 'Blue',
-            data: [80, 20],
+            data: [90, 10],
             backgroundColor: [
-                'rgba(184, 255, 50, 0.2)',
-                'rgba(255, 25, 100, 0.2)'],
+                'rgba(17, 76, 60, 0.5)',
+                'rgba(54, 103, 145, 0.3)'],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 25, 100, 1)'],
+                'rgba(17, 76, 60, 1)',
+                'rgba(54, 103, 145, 1)'],
             borderWidth: 1
         }]
     },
@@ -33,3 +33,135 @@ let chart = new Chart(ctx, {
         }
     }
 });
+
+let projectCtx = document.getElementById('project-management-graph').getContext('2d');
+let projectChart = new Chart(projectCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Proficiency', 'Growth'],
+        datasets: [{
+            label: 'Blue',
+            data: [90, 10],
+            backgroundColor: [
+                'rgba(17, 76, 60, 0.5)',
+                'rgba(54, 103, 145, 0.3)'],
+            borderColor: [
+                'rgba(17, 76, 60, 1)',
+                'rgba(54, 103, 145, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+            legend: {
+                display: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }    
+        }
+    }
+});
+
+let leadershipCtx = document.getElementById('leadership-graph').getContext('2d');
+let leadershipChart = new Chart(leadershipCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Proficiency', 'Growth'],
+        datasets: [{
+            label: 'Blue',
+            data: [95, 5],
+            backgroundColor: [
+                'rgba(17, 76, 60, 0.5)',
+                'rgba(54, 103, 145, 0.3)'],
+            borderColor: [
+                'rgba(17, 76, 60, 1)',
+                'rgba(54, 103, 145, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+            legend: {
+                display: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }    
+        }
+    }
+});
+
+let accountabilityCtx = document.getElementById('accountability-graph').getContext('2d');
+let accountabilityChart = new Chart(accountabilityCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Proficiency', 'Growth'],
+        datasets: [{
+            label: 'Blue',
+            data: [90, 10],
+            backgroundColor: [
+                'rgba(17, 76, 60, 0.5)',
+                'rgba(54, 103, 145, 0.3)'],
+            borderColor: [
+                'rgba(17, 76, 60, 1)',
+                'rgba(54, 103, 145, 1)'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: false,
+            legend: {
+                display: false,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }    
+        }
+    }
+});
+
+  //////////////////////////////////
+ //Charts for hard-skills section//
+//////////////////////////////////
+
+// let htmlCtx = document.getElementById('html-graph').getContext('2d');
+// let htmlChart = new Chart(htmlCtx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ['Proficiency', 'Growth'],
+//         datasets: [{
+//             label: 'Blue',
+//             data: [90, 10],
+//             backgroundColor: [
+//                 'rgba(17, 76, 60, 0.5)',
+//                 'rgba(54, 103, 145, 0.3)'],
+//             borderColor: [
+//                 'rgba(17, 76, 60, 1)',
+//                 'rgba(54, 103, 145, 1)'],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         responsive: false,
+//             legend: {
+//                 display: false,
+//                         scales: {
+//                             yAxes: [{
+//                                 ticks: {
+//                                     beginAtZero: true
+//                                 }
+//                             }]
+//                         }    
+//         }
+//     }
+// });
